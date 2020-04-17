7db7cb7389922ae2bc54a1650ade8aed2acbef73
Sam Shen <slshen@users.noreply.github.com>
2020-04-17 15:19:54 -0700

    Release 0.3.9

--- Casks/soluble.rb
+++ Casks/soluble.rb
@@ -1,6 +1,6 @@
 cask 'soluble' do
-  version '0.3.8'
-  sha256 '3a2b96bbfd64ebec101390fa93ba9ae7ecc812a0eaad39d4df95ebeee50520dc'
+  version '0.3.9'
+  sha256 '250a2c5f53fd7f25330a1a09cbdf6d639545f205a65b0239391a866d0320ad2c'
 
   # storage.googleapis.com/storage/v1/b/soluble-public/o/cli was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/storage/v1/b/soluble-public/o/cli%2Fv#{version}%2Fsoluble_v#{version}_darwin_amd64.tar.gz?alt=media"
