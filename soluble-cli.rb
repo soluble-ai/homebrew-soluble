dc12888dce71b1ff6acf5d8b2a7c5ecfd977515d
Sam Shen <slshen@users.noreply.github.com>
2020-05-07 09:21:35 -0700

    Add non-cask formula

---
+++ soluble-cli.rb
@@ -0,0 +1,20 @@
+class SolubleCli < Formula
+  desc "Command-line interface for Soluble"
+  homepage "https://soluble.ai"
+  url "https://github.com/soluble-ai/soluble-cli/archive/v0.4.1.tar.gz"
+  sha256 "22f6174fab546813175894fdf764a5ef06e93778059ff251dc2666e156e4da8b"
+
+  depends_on "go@1.13" => :build
+
+  def install
+    system "go", "generate"
+    system "go", "build", "-o", "soluble", "-tags", "ci,osusergo,netgo",
+      "-ldflags=-X github.com/soluble-ai/soluble-cli/pkg/version.Version=#{version}"\
+      " -X github.com/soluble-ai/soluble-cli/pkg/version.BuildTime=#{Time.now.iso8601}"
+    bin.install "soluble"
+  end
+
+  test do
+    system "#{bin}/soluble", "version"
+  end
+end
