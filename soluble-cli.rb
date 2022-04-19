686fcb7bfaa243ccc88ed223a193f460056221fd
Hemanth Gokavarapu <hemanth@soluble.ai>
2022-04-19 15:10:46 +0530

    fix sha256 for v0.5.10
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- soluble-cli.rb
+++ soluble-cli.rb
@@ -2,7 +2,7 @@ class SolubleCli < Formula
   desc "Command-line interface for Soluble"
   homepage "https://soluble.ai"
   url "https://github.com/soluble-ai/soluble-cli/archive/v0.5.10.tar.gz"
-  sha256 "9694d0c11f0a0cb7e174a5cc27bdd62681544e9aab913d2cffb864c9039ab119"
+  sha256 "2cfc2788ea555e87c2887b3115e8de2037143bfe580e68d4e517b94ec88f1ca0"
 
   depends_on "go@1.17" => :build
 
