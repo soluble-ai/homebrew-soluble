a93e40bd26a617eb9e2498baba06958493f422d6
Sam Shen <slshen@users.noreply.github.com>
2022-08-08 17:02:29 -0700

    Version v0.5.30

--- soluble-cli.rb
+++ soluble-cli.rb
@@ -1,8 +1,8 @@
 class SolubleCli < Formula
   desc "Command-line interface for Soluble"
   homepage "https://soluble.ai"
-  url "https://github.com/soluble-ai/soluble-cli/archive/v0.5.29.tar.gz"
-  sha256 "87f0d3591f8669017e14c29997b09141fab6bc9ad8013eca42e3a735bbb748c0"
+  url "https://github.com/soluble-ai/soluble-cli/archive/v0.5.30.tar.gz"
+  sha256 "ad0ee25aaf7b471282fe0dcc407828afa245b7926ffd5949ecda8a85779fe6a8"
 
   depends_on "go@1.17" => :build
 
