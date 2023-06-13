ef949cff4b4a64e008d108f85ebac9bf6f7f5e91
Martin Scott <martin.scott@lacework.net>
2023-06-13 17:26:41 +0100

    chore(COD-1130) update to version 0.5.64
    Signed-off-by: Martin Scott <martin.scott@lacework.net>

--- soluble-cli.rb
+++ soluble-cli.rb
@@ -1,8 +1,8 @@
 class SolubleCli < Formula
   desc "Command-line interface for Soluble"
   homepage "https://soluble.ai"
-  url "https://github.com/soluble-ai/soluble-cli/archive/v0.5.63.tar.gz"
-  sha256 "bb02c74e07df635ac88a3059f68a5042bcb25435b6b3d8249e15e712da9dc999"
+  url "https://github.com/soluble-ai/soluble-cli/archive/v0.5.64.tar.gz"
+  sha256 "8ed13ed7516707b4a6ccbaa31bef9bd3f1a5be01129f58e52b7b587fcc4751b3"
   depends_on "go@1.18" => :build
 
   def install
