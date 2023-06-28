0c47c4372ba709764db11c8f796f54b5b3f49471
Martin Scott <martin.scott@lacework.net>
2023-06-28 15:34:34 +0100

    chore(COD-1252) update cask version

--- Casks/soluble.rb
+++ Casks/soluble.rb
@@ -1,11 +1,10 @@
 cask 'soluble' do
-  version '0.3.11'
-  sha256 'e80693aca0542e1fded65acc38e509e1f568f1296f97a466e61aaade36e81b6f'
+  version '0.5.64'
+  sha256 'fe2ed6cbb12a717a15a4e0a2d62057000d06611ec9e93499074db059d435b1a2'
 
-  # storage.googleapis.com/storage/v1/b/soluble-public/o/cli was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/storage/v1/b/soluble-public/o/cli%2Fv#{version}%2Fsoluble_v#{version}_darwin_amd64.tar.gz?alt=media"
-  name 'Soluble Command Line Interface'
-  homepage 'https://www.soluble.ai/'
+  url "https://github.com/soluble-ai/soluble-cli/releases/download/v#{version}/soluble_v#{version}_darwin_amd64.tar.gz"
+  name 'Lacework Soluble Command Line Interface'
+  homepage 'https://www.lacework.com/'
 
   binary 'soluble'
 
