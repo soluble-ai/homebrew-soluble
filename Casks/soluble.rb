e869c2994d07a5e75f7bd93b9c9ee9933edd0fb5
Ellie <81188241+ellielacework@users.noreply.github.com>
2023-07-06 16:30:18 +0100

    Update soluble.rb
    v0.6.2

--- Casks/soluble.rb
+++ Casks/soluble.rb
@@ -1,6 +1,6 @@
 cask 'soluble' do
-  version '0.6.1'
-  sha256 '4bdc988d8c70ced76b1791234649276b11c9b4abc9af14decfa47864b95d2369'
+  version '0.6.2'
+  sha256 '7ec28f3af72a9fd6f0c050d975da42dcc567fdde45538e6064b8617fff33b3f3'
 
   url "https://github.com/soluble-ai/soluble-cli/releases/download/v#{version}/soluble_v#{version}_darwin_amd64.tar.gz"
   name 'Lacework Soluble Command Line Interface'
