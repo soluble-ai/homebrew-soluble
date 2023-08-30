d105592dd08e08f08c25e03fea13fc02201af3e3
Martin Scott <martin.scott@lacework.net>
2023-08-30 11:14:40 +0100

    release v0.6.6
    Signed-off-by: Martin Scott <martin.scott@lacework.net>

--- Casks/soluble.rb
+++ Casks/soluble.rb
@@ -1,6 +1,6 @@
 cask 'soluble' do
-  version '0.6.3'
-  sha256 'd424d8e61e026728772c9c475109ff386e332d9bc71a4b3e63e8d80d483a1773'
+  version '0.6.6'
+  sha256 '13a27cce41d568df8cd980bd561b761ad52c0502b78a8fb1f625000733eae05a'
 
   url "https://github.com/soluble-ai/soluble-cli/releases/download/v#{version}/soluble_v#{version}_darwin_amd64.tar.gz"
   name 'Lacework Soluble Command Line Interface'
